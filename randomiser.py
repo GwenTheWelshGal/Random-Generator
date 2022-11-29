0bc6c0d2713830741efca4510ba6e370ab6e55b5
GwenTheWelshGal <105872596+GwenTheWelshGal@users.noreply.github.com>
2022-11-29 22:09:55 +0000

    Minor edits.

--- randomiser.py
+++ randomiser.py
@@ -1,21 +1,25 @@
+"""
+This is a Python file tha will help the randomiser work.
+
+I've made a randomiser once, but not in such a way that it will work for a HTML frontend instead of a terminal.
+"""
 
 
+##### ##### ##### ##### ##### The Lists
 
-
-
-##### The arrays
-
-arrayA = [
+ListA = [
     '1',
     '2',
     '3'
 ]
 
-arrayB = [
+ListB = [
     'a',
     'b',
     'c'
 ]
+
+##### ##### ##### ##### ##### The Functions
 
 def randomise():
     pass
